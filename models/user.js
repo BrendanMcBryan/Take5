f5e28bacc72d8e02cc8c2d822483d924a1d9b52d
Brendan McBryan <brendan.mcbryan@gmail.com>
2019-09-04 20:47:07 -0400

    fixed for travis

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.ARRAY(DataTypes.INTEGER)
     },
 
-    total_likes: {
+    totalLikes: {
       type: DataTypes.INTEGER
     }
   });
