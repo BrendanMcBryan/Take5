0e3fb307a6c54e1a3e12031476087eb8311b4c7a
David Morales <dmoralesx24@gmail.com>
2019-09-09 20:54:42 -0400

    profile page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,15 +11,20 @@ module.exports = function(app) {
     });
   });
 
+  // TODO original route
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  //     dbExample
+  //   ) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
+
+  app.get("/dashboard", function(req, res) {
+    res.render("profile");
   });
 
   // Render 404 page for any unmatched routes
