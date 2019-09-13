4290791e64d5dc605737f4b0a6fc87ff4ea8ae66
Brendan McBryan <brendan.mcbryan@gmail.com>
2019-09-13 09:56:11 -0400

    more travis

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -31,10 +31,7 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/dashboard", isAuthenticated, function(req, res) {
-    
-    
     res.render("profile", req.user);
-
 
     console.log(req.user);
   });
